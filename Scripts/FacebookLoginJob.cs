d64440b7f2a2bcef6ba87cfb792ab69a5edcba48
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-03-11 23:17:11 +0700

    Move facebook login codes from MMO repository

---
+++ Scripts/FacebookLoginJob.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MultiplayerARPG.MMO
+{
+    public class FacebookLoginJob : DatabaseJob<string>
+    {
+        private string id;
+        private string email;
+        public FacebookLoginJob(BaseDatabase database, string id, string email, Action<string> onFinished = null) : base(database, onFinished)
+        {
+            this.id = id;
+            this.email = email;
+        }
+
+        protected override void ThreadFunction()
+        {
+            result = database.FacebookLogin(id, email);
+        }
+    }
+}
