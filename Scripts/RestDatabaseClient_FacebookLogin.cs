281104f1ecb20dde5a2aa63f60a3d4493339d09f
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-10 06:50:19 +0700

    feat: change database client codes follow constructure changes

---
+++ Scripts/RestDatabaseClient_FacebookLogin.cs
@@ -0,0 +1,13 @@
+using Cysharp.Threading.Tasks;
+using LiteNetLibManager;
+
+namespace MultiplayerARPG.MMO
+{
+    public partial class RestDatabaseClient
+    {
+        public UniTask<AsyncResponseData<DbFacebookLoginResp>> RequestDbFacebookLogin(DbFacebookLoginReq request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
